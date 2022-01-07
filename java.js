063968f3a3d5cc5ee898b45d10c199eb4f09b51b
Alexandre Prediam <aprediam@gmail.com>
2022-01-07 17:23:52 +0100

    sauvegarde finale

---
+++ java.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+
+  if (window.scrollY < 800) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-70px";
+  }
+});
